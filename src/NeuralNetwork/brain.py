ef1bda3dc0c28d51bc0676996e900ba22c647934
Henrik Ljunggren <telleport@hotmail.com>
2016-01-08 15:14:43 +0100

    Finished the first working version of the spider brain (neural network)

--- src/NeuralNetwork/brain.py
+++ src/NeuralNetwork/brain.py
@@ -3,31 +3,44 @@ Created on 30 dec. 2015
 
 @author: Henrik
 '''
+import math
+import random
 
-class spiderBrain(object):
+class spiderBrainFullyCon(object):
     '''
     classdocs
     '''
-    webState = []
+
+
     
-    neighbours = 8
-    xSize = 100
-    ySize = 100
-    
-    'weights and bias'
-    webWeights = []
-    webBias = []
-    
-    timeWeight = 0
-    timeBias = 0
     
 
-    def __init__(self, params):
+    def __init__(self, layerStruct):
         '''
         Constructor
+        
+        ex:
+        layerStruc = [4892, 20, 15, 10, 1]   eg. first 4892 neurons (input layer) connected with 20 neurons (hidden layer), connected with 15 neurons (hidden layer), connected with 10 neurons (hidden layer), connected with 1 neuron (output layer)
         '''
+        
+        self.allLayers = []
+        self.layerStruct = layerStruct
+        for layerIndex in range(0,self.layerStruct.__len__()):
+            self.allLayers.append([])
+            
+            if layerIndex==0:
+                inputNumber = 1
+            else:
+                inputNumber = self.layerStruct[layerIndex-1]
+                    
+            for index2 in range(0,self.layerStruct[layerIndex]):
+                self.allLayers[layerIndex].append(perceptron([0]*inputNumber,0))
 
-        self.webState=[[[0]*self.xSize]*self.ySize]*self.neighbours
+
+
+
+
+        '''self.webState=[[[0]*self.xSize]*self.ySize]*self.neighbours
         self.webWeights = [[0]*self.xSize]*self.ySize
         self.webBias = [0]*self.neighbours
         
@@ -39,9 +52,57 @@ class spiderBrain(object):
         print(self.webState[0][0][0])
         print([[[0]*self.ySize]*2]*2)
         
-        print("whatsup")
+        print("whatsup")'''
         
         
+    def crunch(self, inputs):
+        if inputs.__len__()==self.layerStruct[0]:
+            i = 0
+            for layer in self.allLayers:
+                outputs = []
+                if i == 0:
+                    for percept in layer:
+                        outputs.append(percept.activate([inputs.pop()]))
+                    inputs = outputs
+                    i = i+1
+                else:
+                    for percept in layer:
+                        outputs.append(percept.activate(inputs))
+                    inputs = outputs            
+            
+        else:
+            print("Wrong input size")
+        return outputs
+
+        
+    def randomizeAllWeights(self):
+        for layerIndex in range(0,self.layerStruct.__len__()):
+            
+            if layerIndex==0:
+                inputNumber = 1
+            else:
+                inputNumber = self.layerStruct[layerIndex-1]
+                
+            for index2 in range(0,self.layerStruct[layerIndex]):
+                for index3 in range(0,inputNumber):
+                    #'sets all k to a value between -1 and 1'
+                    self.allLayers[layerIndex][index2].k[index3]= (random.random()*2+-1)
+                #'sets b to a value between 0 and 1'
+                self.allLayers[layerIndex][index2].b = random.random()
+        
+        
+        
+        
+    def copyBrainWeights(self,allLayersToCopyFrom):
+        #Children
+        a = 0
+        
+    def randomizeSomeWeights(self):
+        #Mutation
+        d = 0
+        
+    def visualizeActivation(self):
+        d = 0
         
         
         
@@ -52,11 +113,41 @@ class perceptron(object):
     '''
     classdocs
     '''
+    k = []
+    b = 0
 
 
-    def __init__(self, params):
+    def __init__(self, kInput,bInput):
         '''
         Constructor
         '''
+        self.k = kInput
+        self.b = bInput
+        
+    def activate(self, inputVector):
+        t = self.dotProduct(inputVector,self.k)+self.b
+        outputNumber = 1/(1+math.exp(-t))
+       
+        
+        return outputNumber
+        
+    def dotProduct(self,a,b):
+        if (a.__len__() != b.__len__()):
+            print("CANNOT MAKE CORRECT DOT PRODUCT, SIZE MISSMATCH")
+        c = 0
+        for index in range(0,a.__len__()):
+            c+=(a[index]*b[index])
+        return c           
+            
+
+
+def print2DMatrix(matrix):
+
+    s = [[str(e) for e in row] for row in matrix]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    print('\n'.join(table))
+        
         
         
--- src/testPack/helloworld.py
+++ src/testPack/helloworld.py
@@ -3,10 +3,22 @@ Created on 30 dec. 2015
 
 @author: Henrik
 '''
+import random
 
 import NeuralNetwork.brain
 
 if __name__ == '__main__':
-    hej = NeuralNetwork.brain.spiderBrain(1)
-    print("Hello World!")
+
+    #hej =(NeuralNetwork.brain.spiderBrainFullyCon([3,2,1]))
+    #hej.randomizeAllWeights()
+    #print(hej.crunch([1,1,1]))
+    #print("Hello World!")
+    
+    p = []
+    for i in range(0,1000):
+        p.append(NeuralNetwork.brain.spiderBrainFullyCon([400,20,15,1]))
+        p[i].randomizeAllWeights()
+        input2 = [random.random()]*400
+        #print(p[i].crunch(input2))
+        print(i)
     pass
