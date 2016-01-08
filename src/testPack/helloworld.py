470454176e38d01d981183c21cce06f02e026519
Henrik Ljunggren <telleport@hotmail.com>
2016-01-08 21:22:56 +0100

    Run helloworld to see index calc

--- src/testPack/helloworld.py
+++ src/testPack/helloworld.py
@@ -4,15 +4,28 @@ Created on 30 dec. 2015
 @author: Henrik
 '''
 import random
+import time
+import math
 
 import NeuralNetwork.brain
 
 if __name__ == '__main__':
-
-    hej =(NeuralNetwork.brain.spiderBrainFullyCon([3,2,1]))
+    
+    millis = int(round(time.time() * 1000))
+    random.seed(millis)
+    hej =(NeuralNetwork.brain.spiderBrainFullyCon([3,2,8]))
     hej.randomizeAllWeights()
-    hej.randomizeSomeWeights(0.001,0.001)
-    print(hej.crunch([1,1,1]))
+    
+    output = hej.crunch([1,1,1])
+    max_value = max(output)
+    max_index = output.index(max_value)
+    print("max_value = ",round(max_value,3)," max_index = ", max_index)
+    
+    hej.randomizeSomeWeights(0.01,0.0000000001)
+    print(max(hej.crunch([1,1,1])))
+    hej.randomizeSomeWeights(0.1,0.1)
+    print(max(hej.crunch([1,1,1])))
+    
     print("Hello World!")
     
     '''p = []
