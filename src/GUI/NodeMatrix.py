19483db8154ec7c6d6a04e2570e11021759614ca
GustavWi <gustavwiklander@gmail.com>
2016-01-08 19:20:14 +0100

    Working on translating WeightDirectionVector to EndPos

--- src/GUI/NodeMatrix.py
+++ src/GUI/NodeMatrix.py
@@ -10,6 +10,15 @@ Created on 8 jan. 2016
 SpiderWebb is a NodeXCount * NodeYCount * 4 matrix 
 Each node has a connection to right up, right, right down, down ( 3 connections).
 
+  0                     n x-dir
+0|_|_|_|_|_|_|_|_|_|_|_|_|
+ |_|_|_|_|_|_|_|_|_|_|_|_|
+ |_|_|_|_|_|_|_|_|_|_|_|_|
+ |_|_|_|_|_|_|_|_|_|_|_|_|
+ |_|_|_|_|_|_|_|_|_|_|_|_|
+ |_|_|_|_|_|_|_|_|_|_|_|_|
+n|_|_|_|_|_|_|_|_|_|_|_|_|
+y-dir
 '''
 
 class SpiderWebb:
@@ -23,53 +32,55 @@ class SpiderWebb:
         self.NodeXCount = NodeXCount;
         self.NodeYCount = NodeYCount;
         self.Webb = np.zeros((NodeXCount, NodeYCount, 4));
-        
-    #InsertsThread
-    #input:
-    #     Node1Pos = {'x': 3, 'y': 4}
-    #     Node2Pos = {'x': 4, 'y': 5}
-    #output:
-    #     SpiderPos after insert
-    def InsertSpiderThread(self, Node1, Node2):
+    
+    '''
+    InsertSpiderThread
+    input:
+        StartPos = {'x': 3, 'y': 4}
+        EndPos = {'x': 4, 'y': 5}
+    output:
+        SpiderPos after insert
+    '''
+    def InsertSpiderThread(self, StartPos, EndPos):
         try:
-          Xlength = abs(EndPos['x'] - Node1Pos['x'])
-          Ylength = abs(EndPos['y'] - Node1Pos['y'])
+          Xlength = abs(EndPos['x'] - StartPos['x'])
+          Ylength = abs(EndPos['y'] - StartPos['y'])
           if (Xlength > 1 or Ylength > 1 or (Xlength == 0 and Ylength == 0)):
               raise ValueError('Invalid node connection. Xlength: ' + str(Xlength) + ", Ylength: " + str(Ylength));
-          elif (Node1Pos['x'] < 0 or Node1Pos['x'] > (self.NodeXCount-1)):
-              raise ValueError('Index out of bound. Node1Pos[''x'']: ' + Node1Pos['x']);
-          elif (Node1Pos['y'] < 0 or Node1Pos['y'] > (self.NodeYCount-1)):
-              raise ValueError('Index out of bound. Node1Pos[''y'']: ' + Node1Pos['y']);
+          elif (StartPos['x'] < 0 or StartPos['x'] > (self.NodeXCount-1)):
+              raise ValueError('Index out of bound. StartPos[''x'']: ' + StartPos['x']);
+          elif (StartPos['y'] < 0 or StartPos['y'] > (self.NodeYCount-1)):
+              raise ValueError('Index out of bound. StartPos[''y'']: ' + StartPos['y']);
           elif (EndPos['x'] < 0 or EndPos['x'] > (self.NodeXCount-1)):
               raise ValueError('Index out of bound. EndPos[''x'']: ' + EndPos['x']);
           elif (EndPos['y'] < 0 or EndPos['y'] > (self.NodeYCount-1)):
-              raise ValueError('Index out of bound. Node1Pos[''x'']: ' + Node1Pos['x']);
+              raise ValueError('Index out of bound. EndPos[''y'']: ' + EndPos['y']);
           else: ## ALL OK
-              self.__insert_spider_thread(Node1Pos, EndPos);
+              self.__insert_spider_thread(StartPos, EndPos);
               return EndPos;
         except ValueError as e:
             print(str(e))
             print('Insert had no effect.')
-            return Node1Pos;
+            return StartPos;
           # self.Webb
           
-    def __insert_spider_thread(self, Node1Pos, EndPos):
-        x_dir = EndPos['x'] - Node1Pos['x'];
-        y_dir = EndPos['y'] - Node1Pos['y'];
+    def __insert_spider_thread(self, StartPos, EndPos):
+        x_dir = EndPos['x'] - StartPos['x'];
+        y_dir = EndPos['y'] - StartPos['y'];
         if(x_dir > 0):
             if(y_dir < 0):
-                self.Webb[Node1Pos['x']][Node1Pos['y']][SpiderWebb.rightup] = 1;
+                self.Webb[StartPos['x']][StartPos['y']][SpiderWebb.rightup] = 1;
             elif(y_dir == 0):
-                self.Webb[Node1Pos['x']][Node1Pos['y']][SpiderWebb.right] = 1;
+                self.Webb[StartPos['x']][StartPos['y']][SpiderWebb.right] = 1;
             else:
-                self.Webb[Node1Pos['x']][Node1Pos['y']][SpiderWebb.rightdown] = 1;
+                self.Webb[StartPos['x']][StartPos['y']][SpiderWebb.rightdown] = 1;
         elif(x_dir == 0):
             if(y_dir < 0):
                 self.Webb[EndPos['x']][EndPos['y']][SpiderWebb.down] = 1;
             elif(y_dir == 0):
                 raise ValueError('Same index of nodes.');
             else:
-                self.Webb[Node1Pos['x']][Node1Pos['y']][SpiderWebb.down] = 1;
+                self.Webb[StartPos['x']][StartPos['y']][SpiderWebb.down] = 1;
         else:
             if(y_dir < 0):
                 self.Webb[EndPos['x']][EndPos['y']][SpiderWebb.rightdown] = 1;
@@ -85,10 +96,41 @@ class SpiderWebb:
                 for connection in connections:
                     WebbList.append(connection)
         return WebbList;
-        
-    def CalcNextEndPos(Node1Pos, )  
     
-                
+    '''
+    input:  StartPos - {'x': n, 'y': h}
+            DirectionWeightVector - [0.1, 0.3, 0.4, 0.12, 0.5, 0.9, 0.01]
+    output: EndPos - {'x': n + t, 'y': h+g}
+    
+        |0|1|2|
+        |7|x|3|
+        |6|5|4|
+    
+    '''
+    def CalcNextEndPos(StartPos, DirectionWeightVector):
+        index = DirectionWeightVector.index(max(DirectionWeightVector))
+        if index == 0:
+            return {'x' : StartPos['x'] - 1, 'y' : StartPos['y'] -1}
+        elif index == 1:
+            return {'x' : StartPos['x'] - 1, 'y' : StartPos['y'] -1}
+        elif index == 2:
+            return {'x' : StartPos['x'] - 1, 'y' : StartPos['y'] -1}
+        elif index == 3:
+            return {'x' : StartPos['x'] - 1, 'y' : StartPos['y'] -1}
+        elif index == 4:
+            return {'x' : StartPos['x'] - 1, 'y' : StartPos['y'] -1}
+        elif index == 5:
+            return {'x' : StartPos['x'] - 1, 'y' : StartPos['y'] -1}
+        elif index == 6:
+            return {'x' : StartPos['x'] - 1, 'y' : StartPos['y'] -1}
+        elif index == 7:
+            return {'x' : StartPos['x'] - 1, 'y' : StartPos['y'] -1}
+        else:
+            raise ValueError('DirectionWeightVector size incorrect. DirectionWeightVector: ' + str(DirectionWeightVector));
+            
+class TestSpiderWebb:
+    def Test():
+      DirectionWeightVector = [0]*8
 Webb = SpiderWebb(5, 5)
 # Webb.Webb[0][0][0] = 3
 # print (Webb.Webb[99][0][0])
