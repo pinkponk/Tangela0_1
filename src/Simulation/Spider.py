d38e9eb5a45d00b708ed1f38cb14ca4d730ef73a
Henrik Ljunggren <telleport@hotmail.com>
2016-01-08 15:21:33 +0100

    Added empty simulation structure

---
+++ src/Simulation/Spider.py
@@ -0,0 +1,17 @@
+'''
+Created on 8 jan. 2016
+
+@author: Henrik
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
